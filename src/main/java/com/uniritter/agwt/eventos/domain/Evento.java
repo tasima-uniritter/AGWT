7ff0f60c1c15dd1edf2e508ee58d2436756bab32
Alex S. Rosa <alexsros@gmail.com>
2017-09-02 10:07:05 -0300

    Ajustes na classe Evento.

--- src/main/java/com/uniritter/agwt/eventos/domain/Evento.java
+++ src/main/java/com/uniritter/agwt/eventos/domain/Evento.java
@@ -1,17 +1,38 @@
 package com.uniritter.agwt.eventos.domain;
 
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.*;
 import java.util.Date;
 
 /**
  * Created by ALU201731064 on 01/09/2017.
  */
+@Entity
 public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private long Id;
+
+    @Column(length = 150, nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private Date dataDoEvento;
 
     public Evento(String nome, Date dataDoEvento){
         setNome(nome);
         setDataDoEvento(dataDoEvento);
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
     }
 
     public String getNome() {
@@ -33,7 +54,8 @@ public class Evento {
     @Override
     public String toString() {
         return "Evento{" +
-                "nome='" + nome + '\'' +
+                "Id=" + Id +
+                ", nome='" + nome + '\'' +
                 ", dataDoEvento=" + dataDoEvento +
                 '}';
     }
